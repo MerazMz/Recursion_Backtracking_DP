d5e555ffa12e31cc63f87f35ea01a2e19d3106b0
Merazz <83541936+MerazMz@users.noreply.github.com>
2026-05-10 17:22:20 +0530

    Time: 11 ms (93.79%) | Memory: 68.5 MB (69.25%) - LeetSync

--- 2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.cpp
+++ 2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.cpp
@@ -1,25 +1,38 @@
 class Solution {
 public:
-    int solve(vector<int>& nums, int target, int idx, vector<int>& dp){
-        int n=nums.size();
-        if(idx==n-1) return 0;
-        if(dp[idx]!=INT_MIN){ //already calculated for that index 
-            return dp[idx];
-        }
-        int result=INT_MIN;
-        for(int j=idx+1;j<n;j++){
-            if(abs(nums[idx]-nums[j])<=target){
-                int temp = 1+solve(nums,target,j,dp);
-                result=max(result,temp);
-            }
-        }
-        return dp[idx]=result;
+    // int memo(vector<int>& nums, int target, int idx, vector<int>& dp){
+    //     int n=nums.size();
+    //     if(idx==n-1) return 0;
+    //     if(dp[idx]!=INT_MIN){ //already calculated for that index 
+    //         return dp[idx];
+    //     }
+    //     int result=INT_MIN;
+    //     for(int j=idx+1;j<n;j++){
+    //         if(abs(nums[idx]-nums[j])<=target){
+    //             int temp = 1+memo(nums,target,j,dp);
+    //             result=max(result,temp);
+    //         }
+    //     }
+    //     return dp[idx]=result;
 
-    }
+    // }
     int maximumJumps(vector<int>& nums, int target) {
         int n=nums.size();
-        vector<int> dp(n+1,INT_MIN);
-        int ans = solve(nums,target,0,dp);
-        return ans<0? -1:ans;
+        // vector<int> dp(n+1,INT_MIN);
+        // int ans = memo(nums,target,0,dp);
+        // return ans<0? -1:ans;
+
+
+        vector<int> t(n+1,INT_MIN);
+        t[n-1]=0;
+        for(int i=n-2;i>=0;i--){
+            for(int j=i+1;j<n;j++){
+                if(abs(nums[i]-nums[j])<=target and nums[j]!=INT_MIN){
+                    int temp = 1+t[j];
+                    t[i]=max(t[i],temp);
+                }
+            }
+        }
+        return t[0]<0? -1:t[0];
     }
 };
