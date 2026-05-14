1ca76a87d296d18ac1f15ff84e084bd1fa39f42c
Merazz <83541936+MerazMz@users.noreply.github.com>
2026-05-14 15:48:03 +0530

    Time: 1 ms (25.58%) | Memory: 26.7 MB (49.61%) - LeetSync

---
+++ 2892-check-if-array-is-good/check-if-array-is-good.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isGood(vector<int>& nums) {
+        int n=nums.size();
+        int maxi = *max_element(nums.begin(),nums.end());
+        if(n!=maxi+1) return false;
+
+        unordered_map<int,int> map;
+        for(int i=0;i<n;i++){
+            map[nums[i]]++;
+        }
+        bool ans=true;
+        for(auto pair:map){
+            if(pair.first==maxi and pair.second!=2) ans=false;
+            if(pair.first!=maxi and pair.second!=1) ans=false;
+        }
+        return ans;
+    }
+};
