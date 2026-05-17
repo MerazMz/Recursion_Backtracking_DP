2dba03976cf9cdbdbf0cbcffae1477e171a74a08
Merazz <83541936+MerazMz@users.noreply.github.com>
2026-05-18 01:14:15 +0530

    Time: 0 ms (100.00%) | Memory: 35.3 MB (85.67%) - LeetSync

--- 1428-jump-game-iii/jump-game-iii.cpp
+++ 1428-jump-game-iii/jump-game-iii.cpp
@@ -1,44 +1,42 @@
 class Solution {
 public:
-    // bool bfs(vector<int>& nums, vector<bool>& vis,int start){
-    //     int n=nums.size();
-    //     queue<int> q;
-    //     if(start>=n or start<0) return false;
-    //     q.push(start);
-    //     vis[start]=true;
-    //     while(!q.empty()){
-    //         int curridx = q.front();
-    //         q.pop();
-    //         int currval = nums[curridx];
-    //         if(currval==0) return true;
-    //         int aheadidx = curridx+currval;
-    //         int previdx = curridx-currval;
-    //         if(aheadidx>=n || aheadidx<0 || previdx)
-    //         if(!vis[aheadidx]){
-    //             q.push(aheadidx);
-    //             vis[aheadidx]=true;
-    //         }
-    //         if(!vis[previdx]){
-    //             q.push(previdx);
-    //             vis[previdx]=true;
-    //         }
-    //     }
-    //     return false;
-    // }
-    bool dfs(vector<int>& nums, vector<bool>& vis, int i){
+    bool bfs(vector<int>& nums, vector<bool>& vis,int idx){
         int n=nums.size();
-        if(i<0 or i>=n) return false;
-        if(vis[i]) return false;
-        vis[i]=true;
-        if(nums[i]==0) return true;
-        int next = i+nums[i];
-        int prev = i-nums[i];
+        queue<int> q;
+        q.push(idx);
+        vis[idx]=true;
 
-        return dfs(nums,vis,next) or dfs(nums,vis,prev);
+        while(!q.empty()){
+            int i = q.front();
+            q.pop();
+            if(nums[i]==0) return true;
+            int next = i+nums[i];
+            int prev = i-nums[i];
+            if(next<n and next>=0 and !vis[next]){
+                vis[next]=true;
+                q.push(next);
+            }
+            if(prev<n and prev>=0 and !vis[prev]){
+                vis[prev]=true;
+                q.push(prev);
+            }
+        }
+        return false;
     }
+    // bool dfs(vector<int>& nums, vector<bool>& vis, int i){
+    //     int n=nums.size();
+    //     if(i<0 or i>=n) return false;
+    //     if(vis[i]) return false;
+    //     vis[i]=true;
+    //     if(nums[i]==0) return true;
+    //     int next = i+nums[i];
+    //     int prev = i-nums[i];
+
+    //     return dfs(nums,vis,next) or dfs(nums,vis,prev);
+    // }
     bool canReach(vector<int>& nums, int start) {
         int n=nums.size();
         vector<bool> vis(n,false);
-        return dfs(nums,vis,start);
+        return bfs(nums,vis,start);
     }
 };
